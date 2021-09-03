be68b27a98381a1b08d648980ecd10553215da97
aazadpanchhi05 <darshiljadav12@gmail.com>
2021-09-03 10:49:53 +0530

    Init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import { Name } from './pages/Name';
+
+function App() {
+  return (
+    <Container>
+      <Name />
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/pages/Name.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Container } from 'react-bootstrap';
+
+export const Name = () => {
+    return (
+        <Container>
+            <h1>Hello There</h1>
+        </Container>
+    )
+}
